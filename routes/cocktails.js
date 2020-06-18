00530bad2eee89ff06e23c682801812b540f6021
ygoldfrid <ygoldfrid@gmail.com>
2020-06-18 18:01:59 -0500

    Populating fixed

--- routes/cocktails.js
+++ routes/cocktails.js
@@ -6,14 +6,24 @@ const validateObjectId = require("../middleware/validateObjectId");
 const validateBody = require("../middleware/validateBody");
 
 router.get("/", async (req, res) => {
-  const cocktails = await Cocktail.find().sort("name").select("-__v");
+  const cocktails = await Cocktail.find()
+    .sort("name")
+    .select("-__v")
+    .populate("components.ingredient", "_id name");
   res.send(cocktails);
 });
 
 router.get("/:id", validateObjectId, async (req, res) => {
   const cocktail = await Cocktail.findById(req.params.id)
     .select("-__v")
-    .populate("components.ingredient", "_id name image measure alternatives");
+    .populate({
+      path: "components.ingredient",
+      select: "_id name image measure alternatives",
+      populate: {
+        select: "_id name image",
+        path: "alternatives",
+      },
+    });
   if (!cocktail) return res.status(404).send("Cocktail not found");
 
   res.status(200).send(cocktail);
