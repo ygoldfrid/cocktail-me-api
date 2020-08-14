e868bfdf41f49ed61ee9eb8654684c1b639d4462
ygoldfrid <ygoldfrid@gmail.com>
2020-08-14 18:19:17 -0500

    Bug fix in authentication

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,14 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
+  let user = await User.findOne({ email: req.body.email.toLowerCase() });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  user = new User({
+    name: req.body.name,
+    email: req.body.email.toLowerCase(),
+    password: req.body.password,
+  });
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
